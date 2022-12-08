cb5ee5eb8d6bba5e0f401c97e7ce83d6b52fef6b
snaag <snaag.dev@gmail.com>
2022-12-09 04:36:53 +0900

    first commit

---
+++ src/test/java/snaag/company/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package snaag.company.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
